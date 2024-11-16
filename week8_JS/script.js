77535ff2529353cbb6a4e5c708048b8905a0e937
Duy Hung <22070760@vnu.edu.vn>
2024-11-16 18:51:01 +0700

    init commit

---
+++ week8_JS/script.js
@@ -0,0 +1,5 @@
+function calculate() {
+    var a = document.getElementById("a_value").value;
+    var result = parseInt(a) * 2;
+    document.getElementById("result").innerHTML = "Result: " + result;
+}
